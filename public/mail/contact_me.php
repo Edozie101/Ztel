89fc3d42d40aeb624b45ffa698222b94c53ec7ef
edzye101 <edzye101@gmail.com>
2015-11-08 18:12:48 +0000

    getting ready to push to heroku after adding heroku sendgrid addon

--- public/mail/contact_me.php
+++ public/mail/contact_me.php
@@ -9,11 +9,42 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-
+$url = 'https://api.sendgrid.com/';
+$user = 'XXXXXXXX';
+$pass = 'XXXXXXXX';
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+
+$params = array(
+  'api_user'  => $user,
+  'api_key'   => $pass,
+  'to'        => 'edzye101@gmail.com',
+  'subject'   => 'Contact from ZegTel',
+  'html'      => $first_name,
+  'text'      => $message,
+  'from'      => $email_address,
+)
+
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+
+// print everything out
+print_r($response);
 
 // Create the email and send the message
 $to = 'edzye101@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
@@ -21,6 +52,11 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: edzye101@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
+
+
+
+
+
 if (mail($to,$email_subject,$email_body,$headers)) {
   return true;
 }else {
