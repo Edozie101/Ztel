372e34fe7f388bcaf6eb8851674997488bec0f42
edzye101 <edzye101@gmail.com>
2015-11-08 19:08:39 +0000

    Changed to input Pony mail hash not Pony options

--- app.rb
+++ app.rb
@@ -4,22 +4,22 @@ get '/' do
   erb :index
 end
 
-# post '/mail' do
-#   require 'pony'
-#   Pony.options = {
-#   # unless params[:name] == '' || params[:email] == '' || params[:content] == ''
-#     :subject => "Some Subject",
-#     :body => "This is the body.",
-#     :to => "edzye101@gmail.com"
-#     :via => :smtp,
-#     :via_options => {
-#       :address => 'smtp.sendgrid.net',
-#       :port => '587',
-#       :domain => 'heroku.com',
-#       :user_name => ENV['SENDGRID_USERNAME'],
-#       :password => ENV['SENDGRID_PASSWORD'],
-#       :authentication => :plain,
-#       :enable_starttls_auto => true
-#   }
-# }
-# end
+post '/mail' do
+  require 'pony'
+  Pony.mail = ({
+  # unless params[:name] == '' || params[:email] == '' || params[:content] == ''
+    :subject => "Some Subject",
+    :body => "This is the body.",
+    :to => "edzye101@gmail.com",
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+})
+end
