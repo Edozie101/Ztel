efb4def0208567f186db3432fda71afbd2fc4068
edzye101 <edzye101@gmail.com>
2015-11-12 10:41:33 +0000

    Added my environment rb file and put in the activerecord.base

--- Gemfile
+++ Gemfile
@@ -3,5 +3,8 @@ gem 'sinatra'
 gem 'pony'
 gem 'figaro'
 gem 'config_env'
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "pg"
 
 ruby '2.1.3'
---
+++ config/environment.rb
@@ -0,0 +1,16 @@
+configure :production, :development do
+  db = URI.parse(ENV['DATATBASE_URL'] || "postgres://localhost/mydb")
+
+  ActiveRecord.Base.establish_connection (
+              :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+              :host => db.host,
+              :username => db.user,
+              :password => db.password,
+              :database => db.path[1..-1],
+              :encoding => 'uft8'
+
+
+
+
+  )
+end
